86f09650e624ab7a5238f4b8fd5ea75981e0b941
MananTaneja <manantaneja007@gmail.com>
2024-08-20 03:50:27 +0530

    chore: cleanup

--- price-alert/internal/flipkart.go
+++ price-alert/internal/flipkart.go
@@ -1,10 +1,12 @@
 package priceAlert
 
 func FlipkartFetch() {
-	const url = "https://1.rome.api.flipkart.com/api/4/page/fetch"
+	// const url = "https://1.rome.api.flipkart.com/api/4/page/fetch"
 
 	// client := &http.Client{Timeout: 30 * time.Second}
-
-	// req, _ := http.NewRequest("POST")
+	// add page uri to this post request body
+	// req, _ := http.NewRequest("POST", url, )
+	// req.Header.Set("User-Agent", "Mozilla/5.0 (Android 12; Mobile; rv:109.0) Gecko/113.0 Firefox/113.0")
+	// req.Header.Set("Cookie", "cookie_key=cookie_value;")
 
 }
